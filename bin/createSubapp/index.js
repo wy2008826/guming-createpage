692243c78dc4b2b09b9446cc9ac83d920f361e05
wangyu <760104178@qq.com>
2020-05-11 18:09:10 +0800

    子项目生成处理完成

--- bin/createSubapp/index.js
+++ bin/createSubapp/index.js
@@ -9,58 +9,82 @@ const ejs = require('ejs');
 const Rx = require('rxjs');
 const cwd = process.cwd();
 const exec = require('child_process').exec;
+const ora =  require('ora');
+const Tip = require('../../lib/chalkMsg.js');
+const deleteFolder = require('../../lib/deleteFolder.js');
 
-console.log(exec);
+const spinner_clone = ora('拉取模板代码中...')
+const spinner_Install = ora('npm包安装中...')
+
+
+//执行命令行代码
+const execCmd = (cmdText)=> new Promise((resolve,reject)=>{
+    exec(cmdText,(error)=>{
+        if(error){
+            reject(error)
+        }else{
+            resolve(true)
+        }
+        // resolve(error ? false : true)
+    })
+});
 
 program
     .version('0.1.0')
-    .option('-C, --chdir <path>', 'change the working directory')
-    .option('-c, --config <path>', 'set config path. defaults to ./deploy.conf')
-    .option('-T, --no-tests', 'ignore test hook');
+    // .option('-C, --chdir <path>', 'change the working directory')
+
 
 //初始化生成微前端子项目
 program
     .command('init [projectname]')
     .description('初始化微前端子项目')
+    .alias('it')
     .option("-s, --setup_mode [mode]", "Which setup mode to use")
-    .action(function (projectname, options) {
-        const reg = /^[a-zA-Z]{4}$/g;
-        if(!reg.test(projectname)){
-            console.log('项目名称只能包含4位以上大小写字母');
+    .action(async function  (projectname, options) {
+        const projectDir = path.resolve(process.cwd(),`./${projectname}/`);
+        const reg = /^[a-zA-Z]{4,}$/;
+
+        const isExist = fs.existsSync(projectDir);
+        if(isExist){
+            Tip.error(`${projectname} 项目名称已存在 请重新修改项目名称！`);
+            return ;
+        }
+
+        if (!reg.test(projectname)) {
+            Tip.error('项目名称只能包含4位以上大小写字母');
             return;
         }
-        exec(`git clone http://47.96.116.145:8765/wangyu/book-subapp-project-template.git`)
+        spinner_clone.start();
+        let cloneResult = await execCmd(`git clone http://47.96.116.145:8765/wangyu/book-subapp-project-template.git ${projectname}`);
+        spinner_clone.stop();
+
+        if(cloneResult){
+            // fs.rmdir(),(err)=>{
+            //     console.log(err,'git文件删除成功！');
+            // });
+            //修改项目中模板代码
+
+            deleteFolder(path.resolve(process.cwd(),`./${projectname}/`,`.git`));
+
+
+            spinner_clone.stop();
+
+            //修改项目名称
+            let projectConfig = fs.readFileSync(path.resolve(projectDir,'./webpack.project.config.js'),'utf-8');
+            projectConfig = projectConfig.replace(/contract/g,projectname);
+            fs.writeFileSync(path.resolve(projectDir,'./webpack.project.config.js'),projectConfig);
+
+            spinner_Install.start();
+            let npmInstall = await execCmd(`cd ${projectname} && cnpm i`);
+            spinner_Install.stop();
+            Tip.success('项目生成成功！');
+
+        }
     });
 
-// program
-//     .command('setup [env]')
-//     .description('run setup commands for all envs')
-//     .option("-s, --setup_mode [mode]", "Which setup mode to use")
-//     .action(function (env, options) {
-//         const mode = options.setup_mode || "normal";
-//         env = env || 'all';
-//         console.log('setup for %s env(s) with %s mode', env, mode);
-//     });
 
-program
-    .command('exec <cmd>')
-    .alias('ex')
-    .description('execute the given remote cmd')
-    .option("-e, --exec_mode <mode>", "Which exec mode to use")
-    .action(function (cmd, options) {
-        console.log('exec "%s" using %s mode', cmd, options.exec_mode);
-    }).on('--help', function () {
-    console.log('');
-    console.log('Examples:');
-    console.log('');
-    console.log('  $ deploy exec sequential');
-    console.log('  $ deploy exec async');
-});
-
-program
-    .command('*')
-    .action(function (env) {
-        console.log('deploying "%s"', env);
-    });
-
+//如果没有参数，则直接调用帮助命令
 program.parse(process.argv);
+if (!program.args.length) {
+    program.help()
+}
